8972760713f58cd2bc059f231ea040bfe8adcbdf
G00410156@atu.ie <g00410156@atu.ie>
2023-09-27 15:39:08 +0100

    Third Commit

--- src/main/java/ie/atu/hellothere/RequestController.java
+++ src/main/java/ie/atu/hellothere/RequestController.java
@@ -2,6 +2,7 @@ package ie.atu.hellothere;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,11 @@ public class RequestController {
     @GetMapping ("/greet/{name}")
     public String greetbyname(@PathVariable String name){
         return "Hello there, " + name;
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age){
+        return "Hi my name is, " +name + " and I am " + age + " years old";
     }
 
 
